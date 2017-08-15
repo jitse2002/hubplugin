4f9451d7ceac6d26887feca60e00070bf8d84c83
jitse2002 <mastergt.vlogs@gmail.com>
2017-08-15 18:04:09 +0200

    test

--- src/main/java/be/jitsedev/lobby/server/ServerListener.java
+++ src/main/java/be/jitsedev/lobby/server/ServerListener.java
@@ -5,7 +5,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 public class ServerListener implements Listener{
